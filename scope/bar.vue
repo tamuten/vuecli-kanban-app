e11746312efa3b11884dbfe38cb14c7eb30dffc7
tamuten <tamuten310@gmail.com>
2022-03-05 12:07:26 +0000

    #3 third commit

--- scope/bar.vue
+++ scope/bar.vue
@@ -1,7 +1,25 @@
 <template>
-    <div class="bar">
-        <h1 class="header">
-            Barコンポーネント
-        </h1>
-    </div>
+  <div class="bar">
+    <h1 class="header">Barコンポーネント</h1>
+    <p>これはBarコンポーネントです。</p>
+    <foo />
+  </div>
 </template>
+
+<script>
+import Foo from "./foo.vue";
+export default {
+  components: { Foo },
+};
+</script>
+
+<style scope>
+.bar {
+  border: solid 1px red;
+  margin: 4px;
+  padding: 4px;
+}
+.header {
+  font-size: 125%;
+}
+</style>
